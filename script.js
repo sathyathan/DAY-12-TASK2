13aeb99e6197b2f77890e994d3ec9a2b261e8306
sathaythan <sathyathangavel7@gmail.com>
2024-05-02 16:14:26 +0530

    completd

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//dog images
+function getDogImage() {
+  fetch('https://dog.ceo/api/breed/hound/images/random')
+  
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+$(function(){
+    console.log('app loaded');
+    watchForm();
+});
